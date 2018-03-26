1202b0c610beda319666bbdee7da2edfb11d24bd
RJN24 <rnye@ispatechnology.com>
2018-03-26 14:00:17 -0400

    Edited formatting

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
-  user_id       VARCHAR(12)	UNIQUE	NOT NULL,
-  user_pw		VARCHAR(25)			NOT NULL,
-  fname         TEXT				NOT NULL,
-  lname         TEXT				NOT NULL,
-  lv1_correct   INT,
-  lv1_total     INT,
-  lv2_correct   INT,
-  lv2_total     INT,
-  lv3_correct 	INT,
-  lv3_total		INT
+	user_id     VARCHAR(12)	UNIQUE	NOT NULL,
+	user_pw		VARCHAR(25)			NOT NULL,
+	fname       TEXT				NOT NULL,
+	lname       TEXT				NOT NULL,
+	lv1_correct	INT,
+	lv1_total   INT,
+	lv2_correct INT,
+	lv2_total   INT,
+	lv3_correct	INT,
+	lv3_total	INT
 );
