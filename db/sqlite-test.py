a24ebdc5330f439a86a8cfccb17afc8b875b73ab
RJN24 <rnye@ispatechnology.com>
2018-03-27 14:13:29 -0400

    Working on /db/sql-test.py

--- db/sqlite-test.py
+++ db/sqlite-test.py
@@ -1,4 +1,5 @@
 # Doing the same thing that db-test.py does, but using SQLite3
+# This is a work in progress
 
 import sqlite3
 
@@ -9,6 +10,18 @@ cur = conn.cursor()
 cur.execute('''CREATE TABLE users
              (user_id VARCHAR(12) UNIQUE NOT NULL, 
              user_pw VARCHAR(30) NOT NULL, 
-             symbol text, 
-             qty real, 
-             price real)''')
+             fname TEXT, 
+             lname TEXT, 
+             lv1_correct INT DEFAULT 0,
+             lv1_total INT DEFAULT 0,
+             lv2_correct INT DEFAULT 0,
+             lv2_total INT DEFAULT 0,
+             lv3_correct INT DEFAULT 0,
+             lv3_total INT DEFAULT 0)''')
+cur.execute('''INSERT INTO users (user_id, user_pw, fname, lname) VALUES (
+			user_id = 'someUser',
+			user_pw = 'password',
+			fname = 'Bob',
+			lname = 'Smith')
+			''')
+conn.commit()
