fee8793dddd305b544592579cfc1ea92922bdbf5
RJN24 <rnye@ispatechnology.com>
2018-03-28 13:29:31 -0400

    Final edit of sqlite-test.py
    This testing file now tests create, insert, query, update, and drop sqlite3 functions

--- db/sqlite-test.py
+++ db/sqlite-test.py
@@ -1,11 +1,14 @@
 # Doing the same thing that db-test.py does, but using SQLite3
-# This is a work in progress
+# SQLite3 stores a db on disk that persists through sessions
+# This program demonstrates creating a table, inserting a user, querying/updating, and dropping a table
 
 import sqlite3
 
+# Connect to db, this creates a new db if it does not already exist
 conn = sqlite3.connect("test.db")
-
 cur = conn.cursor()
+
+#------------------------------------------ CREATE TABLE ---------------------------------------------------------
 try:
 	cur.execute('''CREATE TABLE users
 	             (id INTEGER PRIMARY KEY,
@@ -23,10 +26,9 @@ except sqlite3.OperationalError as e:
 	print(e)
 	pass
 
-cur.execute('''SELECT * FROM users''')
-all_rows = cur.fetchall()
-print(all_rows)
 
+
+#------------------------------------------ INSERT INTO TABLE ---------------------------------------------------------
 uid = "BobUser"
 pw = "password"
 first = "Bob"
@@ -41,8 +43,76 @@ try:
 			''', (uid, pw, first, last))
 except sqlite3.IntegrityError as e:
 	print(e)
-
 conn.commit()
+
+
+##------------------------------------------ QUERY/UPDATE ---------------------------------------------------------
+cur.execute("SELECT * FROM users")
+print(cur.fetchall())
+
+print("\nBob completed level 1 and scored 8/10... updating table")
+cur.execute("SELECT lv1_correct FROM users WHERE user_id = ?", (uid,))
+correct = cur.fetchone()
+cur.execute("SELECT lv1_total FROM users WHERE user_id = ?", (uid,))
+total = cur.fetchone()
+
+cur.execute("UPDATE users SET lv1_correct = {amt} WHERE {idf} = ?".format(amt = correct[0]+8, idf = "user_id"), (uid,))
+conn.commit()
+
+cur.execute("UPDATE users SET lv1_total = {amt} WHERE {idf} = ?".format(amt = total[0]+10, idf = "user_id"), (uid,))
+conn.commit()
+
+cur.execute("SELECT * FROM users")
+print(cur.fetchall())
+
+cur.execute("SELECT lv1_correct, lv1_total FROM users WHERE user_id = ?", (uid,))
+res = cur.fetchall()
+av = round(res[0][0]/res[0][1], 2)
+print("\nBob's average after scoring 8/10 is {}%".format(av*100))
+
+print("\nHe completed level 1 again and this time scored a perfect score 10/10")
+cur.execute("SELECT lv1_correct FROM users WHERE user_id = ?", (uid,))
+correct = cur.fetchone()
+cur.execute("SELECT lv1_total FROM users WHERE user_id = ?", (uid,))
+total = cur.fetchone()
+
+cur.execute("UPDATE users SET lv1_correct = {amt} WHERE {idf} = ?".format(amt = correct[0]+10, idf = "user_id"), (uid,))
+conn.commit()
+
+cur.execute("UPDATE users SET lv1_total = {amt} WHERE {idf} = ?".format(amt = total[0]+10, idf = "user_id"), (uid,))
+conn.commit()
+
+cur.execute("SELECT * FROM users")
+print(cur.fetchall())
+
+cur.execute("SELECT lv1_correct, lv1_total FROM users WHERE user_id = ?", (uid,))
+res = cur.fetchall()
+av = round(res[0][0]/res[0][1], 2)
+print("\nBob's average after scoring 10/10 is {}%".format(av*100))
+
+print("\nHe completed level 1 one last time and this time scored 3/10... oops!")
+cur.execute("SELECT lv1_correct FROM users WHERE user_id = ?", (uid,))
+correct = cur.fetchone()
+cur.execute("SELECT lv1_total FROM users WHERE user_id = ?", (uid,))
+total = cur.fetchone()
+
+cur.execute("UPDATE users SET lv1_correct = {amt} WHERE {idf} = ?".format(amt = correct[0]+3, idf = "user_id"), (uid,))
+conn.commit()
+
+cur.execute("UPDATE users SET lv1_total = {amt} WHERE {idf} = ?".format(amt = total[0]+10, idf = "user_id"), (uid,))
+conn.commit()
+
+cur.execute("SELECT * FROM users")
+print(cur.fetchall())
+
+cur.execute("SELECT lv1_correct, lv1_total FROM users WHERE user_id = ?", (uid,))
+res = cur.fetchall()
+av = round(res[0][0]/res[0][1], 2)
+print("\nBob's average after scoring 3/10 is {}%".format(av*100))
+
+
+#------------------------------------------ DROP TABLE ---------------------------------------------------------
+cur.execute("DROP TABLE users")
 
 cur.close()
 conn.close()
