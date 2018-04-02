b28e73804e07d2b4c3db0eb38e6efdcffd30fc2e
RJN24 <rnye@ispatechnology.com>
2018-04-02 16:10:41 -0400

    test-login now tests logging in and querying user data

--- db/test-login.py
+++ db/test-login.py
@@ -1,3 +1,7 @@
+# This program will test the functions used in the db.py file
+# Enter y when asked to wipe the db to clear useless test data
+# Leave data in db if you want data to persist between tests
+
 import db
 
 dbfile = "users.db"
@@ -57,5 +61,48 @@ while(exit == 0):
 	else:
 		print("Invalid input. Please enter y, n, or exit.\n")
 
+print("\nNow the test for updating user scores")
+print("Enter 'exit' at any time to exit")
+exit = 0
+while exit == 0:
+	user = input("\nEnter user to update score for: ")
+	if user == "exit":
+		exit = 1
+		break
+	
+	if db.findUser(user):
+		level = input("Enter level to update (1, 2, 3): ")
+		if level == "exit":
+			exit = 1
+			break
+		while int(level) != 1 and int(level) != 2 and int(level) != 3:
+			print("\nInvalid input, please input a level number from 1 to 3")
+			level = input("Enter level to update (1, 2, 3): ")
+			if level == "exit":
+				exit = 1
+				break
+		correct = input("\nEnter number correct out of 10: ")
+		if correct == "exit":
+			exit = 1
+			break
+		while int(correct) > 10 or int(correct) < 0:
+			print("\nInvalid input, please enter a number from 1 to 10")
+			correct = input("Enter number correct out of 10: ")
+		db.updateUserScore(user, int(level), int(correct))
+		print("\nThe average for {user} for level {lvl} is {av}".format(user = user, lvl = level, av = db.getUserAverage(user, int(level))))
+		print(db.getUserInfo(user))
+
+	else:
+		print("No user found under that username\n")
+		cont = input("continue (y/n)?: ")
+		if cont != 'y' or cont != 'y':
+			exit = 1
+			break
+
+
+delete = input("\nEnter y to wipe database, enter anything else to terminate test: ")
+if delete == "y" or delete == "Y":
+	db.deleteUsers()
+	print("Database wiped")
 print("Goodbye")
 db.closeDb()
