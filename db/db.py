c3403ae5474eb3fa02abaa725e7f934b2ba65741
RJN24 <rnye@ispatechnology.com>
2018-04-02 16:10:57 -0400

    Added more functions to db.py

--- db/db.py
+++ db/db.py
@@ -1,6 +1,6 @@
+# This file contains all functions in relation to our db
+
 import sqlite3
-import sys
-from cryptography.fernet import Fernet
 
 def createDbConnection(db_file):
 	try:
@@ -34,10 +34,7 @@ def insertUser(uid, password):
 		present = cur.fetchone()
 		if present is not None:
 			return False
-		cipher_suite = Fernet(key)
-		encode_string = str.encode(password)
-		upw = cipher_suite.encrypt(encode_string)
-		cur.execute("INSERT INTO users (user_id, user_pw) VALUES (?, ?)", (uid, upw))
+		cur.execute("INSERT INTO users (user_id, user_pw) VALUES (?, ?)", (uid, password))
 		conn.commit()
 	except sqlite3.IntegrityError:
 		print(e)
@@ -48,23 +45,80 @@ def verifyLogin(uid, pw):
 	if present is None:
 		return False
 	cur.execute("SELECT user_pw FROM users WHERE user_id = ?", (uid,))
-	one = cur.fetchone()
-	decrypted = Fernet(key)
-	realpw = decrypted.decrypt(one[0])
-	pwstrip = str(realpw).replace("'", "")
-	upw = pwstrip[1:]
-	if pw == upw:
+	upw = cur.fetchone()
+	if pw == upw[0]:
 		return True
 	else:
 		return False
 
-def closeDb():
+def findUser(uid):
+	cur.execute("SELECT * FROM users WHERE user_id = ?", (uid,))
+	present = cur.fetchone()
+	if present is None:
+		return False
+	return True
+
+def getUserInfo(uid):
+	cur.execute("SELECT * FROM users WHERE user_id=?", (uid,))
+	show_me = cur.fetchone()
+	return show_me
+
+def deleteUsers():
 	cur.execute("DROP TABLE IF EXISTS users")
 	conn.commit()
+
+def closeDb():
 	cur.close()
 	conn.close()
 
-# def updateUserScore(uid, lvl, correct):
+def updateUserScore(uid, lvl, result):
+	if lvl == 1:
+		cur.execute("SELECT lv1_correct FROM users WHERE user_id = ?", (uid,))
+		correct = cur.fetchone()
+		cur.execute("SELECT lv1_total FROM users WHERE user_id = ?", (uid,))
+		total = cur.fetchone()
+		cur.execute("UPDATE users SET lv1_correct = {amt} WHERE {idf} = ?".format(amt = correct[0]+result, idf = "user_id"), (uid,))
+		conn.commit()
+		cur.execute("UPDATE users SET lv1_total = {amt} WHERE {idf} = ?".format(amt = total[0]+10, idf = "user_id"), (uid,))
+		conn.commit()
+
+	elif lvl == 2:
+		cur.execute("SELECT lv2_correct FROM users WHERE user_id = ?", (uid,))
+		correct = cur.fetchone()
+		cur.execute("SELECT lv2_total FROM users WHERE user_id = ?", (uid,))
+		total = cur.fetchone()
+		cur.execute("UPDATE users SET lv2_correct = {amt} WHERE {idf} = ?".format(amt = correct[0]+result, idf = "user_id"), (uid,))
+		conn.commit()
+		cur.execute("UPDATE users SET lv2_total = {amt} WHERE {idf} = ?".format(amt = total[0]+10, idf = "user_id"), (uid,))
+		conn.commit()
+
+	elif lvl == 3:
+		cur.execute("SELECT lv3_correct FROM users WHERE user_id = ?", (uid,))
+		correct = cur.fetchone()
+		cur.execute("SELECT lv3_total FROM users WHERE user_id = ?", (uid,))
+		total = cur.fetchone()
+		cur.execute("UPDATE users SET lv3_correct = {amt} WHERE {idf} = ?".format(amt = correct[0]+result, idf = "user_id"), (uid,))
+		conn.commit()
+		cur.execute("UPDATE users SET lv3_total = {amt} WHERE {idf} = ?".format(amt = total[0]+10, idf = "user_id"), (uid,))
+		conn.commit()
+
+def getUserAverage(uid, lvl):
+	if lvl == 1:
+		cur.execute("SELECT lv1_correct FROM users WHERE user_id = ?", (uid,))
+		correct = cur.fetchone()
+		cur.execute("SELECT lv1_total FROM users WHERE user_id = ?", (uid,))
+		total = cur.fetchone()
+	elif lvl == 2:
+		cur.execute("SELECT lv2_correct FROM users WHERE user_id = ?", (uid,))
+		correct = cur.fetchone()
+		cur.execute("SELECT lv2_total FROM users WHERE user_id = ?", (uid,))
+		total = cur.fetchone()
+	elif lvl == 3:
+		cur.execute("SELECT lv3_correct FROM users WHERE user_id = ?", (uid,))
+		correct = cur.fetchone()
+		cur.execute("SELECT lv3_total FROM users WHERE user_id = ?", (uid,))
+		total = cur.fetchone()
+	return correct[0]/total[0]*100
 
 # I may not need this sutff below
 conn = createDbConnection("users.db")
@@ -73,4 +127,3 @@ if (conn is None):
 	sys.exit()
 
 cur = conn.cursor()
-key = Fernet.generate_key()
