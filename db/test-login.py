5557104a96a93d1144ab13655380b381a2af9147
RJN24 <35468944+RJN24@users.noreply.github.com>
2018-04-11 11:59:30 -0400

    Syntax changes (made it more pythonic)

--- db/test-login.py
+++ db/test-login.py
@@ -4,105 +4,104 @@
 
 import db
 
-dbfile = "users.db"
-db.createDbConnection(dbfile)
-db.createTable()
+db_file = "users.db"
+conn = db.create_db_connection(db_file)
+cur = conn.cursor()
+db.create_table(cur, conn)
 
-exit = 0
+exit_program = 0
 
 print("Welcome to the db login test")
 print("Enter 'exit' at any time to exit the test.\n")
-while(exit == 0):
-	check_login = input("Do you have an account (y/n)?: ")
-	if check_login == "y" or check_login == "Y":
-		user = input("username: ")
-		if user == "exit":
-			exit = 1
-			break
-		pw = input("password: ")
-		if pw == "exit":
-			exit = 1
-			break
-		if db.verifyLogin(user, pw):
-			print("Successfully logged in. Welcome, {}!".format(user))
-			break
-		else:
-			print("Invalid username or password\n")
+while exit_program == 0:
+    check_login = input("Do you have an account (y/n)?: ")
+    if check_login == "y" or check_login == "Y":
+        user = input("username: ")
+        if user == "exit":
+            exit_program = 1
+            break
+        pw = input("password: ")
+        if pw == "exit":
+            exit_program = 1
+            break
+        if db.verify_login(cur, user, pw):
+            print("Successfully logged in. Welcome, {}!".format(user))
+            break
+        else:
+            print("Invalid username or password\n")
+    elif check_login == "n" or check_login == "N":
+        print("Let's create an account then.")
+        setUID = input("Enter a username: ")
+        if setUID == "exit":
+            exit_program = 1
+            break
+        setPW = input("Enter a password: ")
+        if setPW == "exit":
+            exit_program = 1
+            break
+        while not db.insert_user(cur, conn, setUID, setPW):
+            print("Username taken, please choose a different username")
+            setUID = input("Enter a username: ")
+            if setUID == "exit":
+                exit_program = 1
+                break
+            setPW = input("Enter a password: ")
+            if setPW == "exit":
+                exit_program = 1
+                break
+            db.insert_user(cur, conn, setUID, setPW)
+        print("Account created, try logging in!\n")
+    elif check_login == "exit":
+        exit_program = 1
+        break
 
-
-	elif check_login == "n" or check_login == "N":
-		print("Let's create an account then.")
-		setUID = input("Enter a username: ")
-		if setUID == "exit":
-			exit = 1
-			break
-		setPW = input("Enter a password: ")
-		if setPW == "exit":
-			exit = 1
-			break
-		while db.insertUser(setUID, setPW) == False:
-			print("Username taken, please choose a different username")
-			setUID = input("Enter a username: ")
-			if setUID == "exit":
-				exit = 1
-				break
-			setPW = input("Enter a password: ")
-			if setPW == "exit":
-				exit = 1
-				break
-			db.insertUser(setUID, setPW)
-		print("Account created, try loggin in!\n")
-
-
-	elif check_login == "exit":
-		exit = 1
-		break
-
-	else:
-		print("Invalid input. Please enter y, n, or exit.\n")
+    else:
+        print("Invalid input. Please enter y, n, or exit.\n")
 
 print("\nNow the test for updating user scores")
 print("Enter 'exit' at any time to exit")
-exit = 0
-while exit == 0:
-	user = input("\nEnter user to update score for: ")
-	if user == "exit":
-		exit = 1
-		break
-	
-	if db.findUser(user):
-		level = input("Enter level to update (1, 2, 3): ")
-		if level == "exit":
-			exit = 1
-			break
-		while int(level) != 1 and int(level) != 2 and int(level) != 3:
-			print("\nInvalid input, please input a level number from 1 to 3")
-			level = input("Enter level to update (1, 2, 3): ")
-			if level == "exit":
-				exit = 1
-				break
-		correct = input("\nEnter number correct out of 10: ")
-		if correct == "exit":
-			exit = 1
-			break
-		while int(correct) > 10 or int(correct) < 0:
-			print("\nInvalid input, please enter a number from 1 to 10")
-			correct = input("Enter number correct out of 10: ")
-		db.updateUserScore(user, int(level), int(correct))
-		print("\nThe average for {user} for level {lvl} is {av}".format(user = user, lvl = level, av = db.getUserAverage(user, int(level))))
-		print(db.getUserInfo(user))
+exit_program = 0
+while exit_program == 0:
+    user = input("\nEnter user to update score for: ")
+    if user == "exit":
+        exit_program = 1
+        break
 
-	else:
-		print("No user found under that username\n")
-		cont = input("continue (y/n)?: ")
-		if cont != 'y' or cont != 'y':
-			exit = 1
-			break
+    if db.find_user(cur, user):
+        level = input("Enter level to update (1, 2, 3): ")
+        if level == "exit":
+            exit_program = 1
+            break
+        while int(level) != 1 and int(level) != 2 and int(level) != 3:
+            print("\nInvalid input, please input a level number from 1 to 3")
+            level = input("Enter level to update (1, 2, 3): ")
+            if level == "exit":
+                exit_program = 1
+                break
+        correct = input("\nEnter number correct out of 10: ")
+        if correct == "exit":
+            exit_program = 1
+            break
+        while int(correct) > 10 or int(correct) < 0:
+            print("\nInvalid input, please enter a number from 1 to 10")
+            correct = input("Enter number correct out of 10: ")
+        db.update_user_score(cur, conn, user, int(level), int(correct))
+        print("\nThe average for {user} for level {lvl} is {av}".format(user=user, lvl=level,
+                                                                        av=db.get_user_average(cur, user, int(level))))
+        print(db.get_user_info(cur, user))
 
+    else:
+        print("No user found under that username\n")
+        cont = input("continue (y/n)?: ")
+        if cont != 'y' or cont != 'y':
+            exit_program = 1
+            break
 
 delete = input("\nEnter y to wipe database, enter anything else to terminate test: ")
 if delete == "y" or delete == "Y":
-	db.deleteUsers()
-	print("Database wiped")
+    db.delete_users(cur, conn)
+    print("Database wiped")
 print("Goodbye")
-db.closeDb()
+
+cur.close()
+conn.close()
