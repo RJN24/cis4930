75563b8e521b7e21684ee7f95829e5dc02a01f2f
RJN24 <rnye@ispatechnology.com>
2018-04-23 13:45:54 -0400

    Added an onclick function for results button
    Also uses ajax to call python file

--- static/js/home.js
+++ static/js/home.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
     $('#FracSolverComponent').hide();
     $('#fracPracticeButton').on('click', function() {
         location.href = '/fraction_practice.html';
-    })
+    });
 
     $('#RedFracButton').on('click', function() {
         location.href = '/reduction_practice.html';
-    })
+    });
 
     $('#fracSolverButton').on('click', function() {
         $('#homeComponent').hide();
         $('#FracSolverComponent').show();
-    })
+    });
 
     $('#EnterFrac').on('click', function() {
         $.ajax({
@@ -26,7 +26,26 @@ $(document).ready(function(){
                     //fractionSolver();
                 }
         })
-    })
+    });
+
+    $('#Results').on('click', function() {
+        location.href = '/results';
+        let user = JSON.parse(localStorage.getItem('userdata'));
+        $.ajax({
+            url: '/get_results',
+            data: JSON.stringify(user),
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function(response) {
+                console.log('submitted');
+            },
+            error: function(error) {
+                console.log('ERROR');
+                console.log(error)
+            }
+        });
+    });
 
     $('#logout').on('click', function() {
         localStorage.clear();
