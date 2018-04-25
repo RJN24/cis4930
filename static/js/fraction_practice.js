fde0ec1033f38827deda6d2342a0074b97252185
RJN24 <rnye@ispatechnology.com>
2018-04-25 10:32:51 -0400

    Reverted changes

--- static/js/fraction_practice.js
+++ static/js/fraction_practice.js
@@ -1,49 +1,60 @@
 $(document).ready(function(){
-
-
-
-	let questions = {}, final_results = {};
-	let numCorrect = null;
+	let questions = {}, results_per_level = {}, final_results = {};
+	let limit = null;
+	let numCorrect = 0;
 	let operation = null, level = null;
 
+
 	$('#fraction_practice').hide();
+	$('#select_operator').hide();
 
 	$('#easy').on('click', function(){
-	    $('span').text("");
-	    questions = {};
 		//console.log('easy');
+		limit = 10;
 		level = "easy";
 		$('#questions').find( "label" ).each(function(elem,index) {
-		   $(this).text(generateEquationsWithAnswers(level, 10));
-		});
-		$('#fraction_practice').show();
-		$('#questions').trigger("reset");
-		$('#display_results').text("");
-		console.log(questions);
-	});
-	$('#medium').on('click', function(){
-		question = {};
-		level = "medium";
-		$('#questions').find( "label" ).each(function(elem,index) {
-		   $(this).text(generateEquationsWithAnswers(level, 10));
-		});
-		$('#fraction_practice').show();
-		$('span').text("");
-		$('#questions').trigger("reset");
-		$('#display_results').text("");
-	});
-	$('#hard').on('click', function(){
-	    $('span').text("");
-	    questions = {};
-		//console.log('hard');
-		level = "hard";
-		$('#questions').find( "label" ).each(function(elem,index) {
-		   $(this).text(generateEquationsWithAnswers(level, 20));
+		   $(this).text(generateEquationsWithAnswers(limit));
 		});
 		$('#fraction_practice').show();
 		$('label').css("background-color", "");
 		$('#questions').trigger("reset");
 		$('#display_results').text("");
+		results_per_level = {};
+	});
+	$('#medium').on('click', function(){
+		//console.log('med');
+		limit = 20;
+		level = "medium";
+		$('#questions').find( "label" ).each(function(elem,index) {
+		   $(this).text(generateEquationsWithAnswers(limit));
+		});
+		$('#fraction_practice').show();
+		$('label').css("background-color", "");
+		$('#questions').trigger("reset");
+		$('#display_results').text("");
+		results_per_level = {};
+	});
+	$('#hard').on('click', function(){
+		//console.log('hard');
+		limit = 30;
+		level = "hard";
+		$('#questions').find( "label" ).each(function(elem,index) {
+		   $(this).text(generateEquationsWithAnswers(limit));
+		});
+		$('#fraction_practice').show();
+		$('label').css("background-color", "");
+		$('#questions').trigger("reset");
+		$('#display_results').text("");
+		results_per_level = {};
+
+	});
+
+	$('#level_button').on('click', function(){
+		$('#fraction_practice').hide();
+		$('#select_operator').hide();
+		$('#select_difficulty').show();
+		results_per_level = {};
+		final_results = {};
 	});
 
 	function gcd_rec(a, b) {
@@ -56,47 +67,29 @@ $(document).ready(function(){
 
 	function operators(op, a, b, c, d) {
         if (op == '+') {
-            //return Number(a / b) + Number(c / d);
-            return (new Fraction(a,b)).add(new Fraction(c,d));
+            return Number(a / b) + Number(c / d);
         }else if (op == '-'){
-            //return Number(a/b) - Number(c/d)
-            return (new Fraction(a,b)).subtract(new Fraction(c,d));
+            return Number(a/b) - Number(c/d)
         }else if (op == '*') {
-            //return Number(a / b) * Number(c / d);
-            return (new Fraction(a,b)).multiply(new Fraction(c,d));
+            return Number(a / b) * Number(c / d);
         }else {
-            //return Number(a / b) / Number(c / d);
-            return (new Fraction(a,b)).divide(new Fraction(c,d));
+            return Number(a / b) / Number(c / d);
         }
     }
 
-	function generateEquationsWithAnswers(level, limit){
-	    let op_list = ['+', '-', '*', '/'];
-        let numerator1, denominator1, numerator2, denominator2;
+	function generateEquationsWithAnswers(limit){
+		//generate fraction
+		let numerator1 = Math.floor(Math.random() * limit) + 1;
+		let denominator1 = Math.floor(Math.random() * limit) + 1;
+		let numerator2 = Math.floor(Math.random() * limit) + 1;
+		let denominator2 = Math.floor(Math.random() * limit) + 1;
 
-	    if (level === "easy"){
-	        denominator1 = Math.floor(Math.random() * (9 - 1) + 1);
-	        denominator2 = denominator1;
-            numerator1 = Math.floor(Math.random() * (9 - 1) + 1);
-            numerator2 = Math.floor(Math.random() * (9 - 1) + 1);
-
-
-	    }
-	    else if (level === "medium"){
-	        denominator1 = Math.floor(Math.random() * (9 - 1) + 1);
-	        denominator2 = Math.floor(Math.random() * (9 - 1) + 1);
-            numerator1 = Math.floor(Math.random() * (9 - 1) + 1);
-            numerator2 = Math.floor(Math.random() * (9 - 1) + 1);
-	    }
-	    else{
-	        denominator1 = Math.floor(Math.random() * limit) + 10;
-	        denominator2 = Math.floor(Math.random() * limit) + 10;
-            numerator1 = Math.floor(Math.random() * limit) + 10;
-            numerator2 = Math.floor(Math.random() * limit) + 10;
-	    }
-
+		let op_list = ['+', '-', '*', '/'];
+		console.log(op_list);
 		let randop = Math.floor(Math.random() * 4);
+		console.log(randop);
 		let op = op_list[randop];
+		console.log(op);
 
 		let question = numerator1 + '/' + denominator1 + " " + op + " " + numerator2 + '/' + denominator2;
 		let answer = operators(op, numerator1, denominator1, numerator2, denominator2);
@@ -105,114 +98,68 @@ $(document).ready(function(){
 		return question;
 	}
 
-
-	function reduced(a){
-		let gcf = gcd_rec(a.numerator, a.denominator);
+	function reduced(a, b){
+		let gcf = gcd_rec(a, b);
+		//console.log(gcf);
 		if(gcf != 1){
 			return false;
 		}
 		return true;
 	}
 
-    function equalFractions(a,b){
-        return a.numerator === b.numerator && a.denominator === b.denominator;
-    }
-
-    function printCorrectAnswer(a){
-        if(a.denominator != 1){
-            return a.numerator + "/" +a.denominator;
-        }
-        else
-            return a.numerator;
-
-    }
-
-	$('#save').on('click', function(event) {
-		numCorrect = 0;
-        let submit_form = true;
-
+	$('#save').on('click', function() {
 		$('#questions').find("input[type=text]").each(function(elem,index) {
-		    let user_answer = null;
-            $this = $(this);
-            $label = $('label[for="'+ $this.attr('id') +'"]');
-            $span = $('span[for="'+ $this.attr('id') +'"]');
-            let correct_answer = questions[$label.text()];
+			//console.log($(this).val());
+			let user_answer = $(this).val().match(/(-?\d+)(?=\/{1}(-?\d+)|)/).slice(1).map(Number);
 
-			if ($(this).val() === ''){ //empty field
-			    //tell user invalid response
-			    $span.text("*Please provide an answer").css("color", "red");
-			    submit_form = false;
-			    return;
+			$this = $(this);
+			$label = $('label[for="'+ $this.attr('id') +'"]');
+			if ($label.length > 0 ) {
+				if(user_answer[1] === NaN && questions[$label.text()] === user_answer[0]){
+					//console.log('correct');
+					$label.css("background-color", "green");
+					numCorrect += 1;
+				}
+				else if(user_answer[1] === 1 || !reduced(user_answer[0], user_answer[1])){
+					//console.log('not reduced');
+					$label.css("background-color", "yellow");
+				}
+				else if (questions[$label.text()] === user_answer[0]/user_answer[1]){
+					//console.log("correct");
+					$label.css("background-color", "green");
+					numCorrect += 1;
+				}
+				else{
+					//console.log("incorrect");
+					$label.css("background-color", "red");
+				}
 			}
-
-			try{
-			    user_answer = $(this).val().match(/(-?\d+)(?=\/{1}(-?\d+)|)/).slice(1).map(Number);
-            }
-            catch{
-                $span.text("*Please enter a fraction or integer for an answer").css("color", "red");
-                submit_form = false;
-			    return;
-            }
-
-            if(isNaN(user_answer[1])){ //one integer for answer
-                if(correct_answer.denominator === 1 && user_answer[0] === correct_answer.numerator){
-                    console.log("correct");
-                    $span.text("Correct").css("color", "green");
-                    numCorrect += 1;
-                }
-                else{
-                    console.log("incorrect");
-                    $span.text("Wrong, Correct answer: " + printCorrectAnswer(correct_answer)).css('color', 'red');
-                }
-            }
-            else{
-                user_answer = { numerator: user_answer[0], denominator: user_answer[1] };
-                if(user_answer.denominator === 1 || (user_answer.numerator/user_answer.denominator ===  correct_answer.numerator/correct_answer.denominator && !reduced(user_answer))){
-                    console.log("not reduced");
-                    $span.text("Not Reduced").css("color", "yellow").css("background-color", "SeaShell");;
-                }
-                else if(equalFractions(user_answer, correct_answer)){
-                    console.log("correct");
-                    $span.text("Correct").css("color", "green");
-                    numCorrect += 1;
-                }
-                else{
-                    console.log("incorrect");
-                    $span.text("Wrong, Correct answer: " + printCorrectAnswer(correct_answer)).css('color', 'red');
-                }
-            }
-
-
-
-
-
 
 		});
 
-        if(submit_form != false){
-            let user = JSON.parse(localStorage.getItem('userdata'));
-            let uid = user.uid;
-            let final_results = {"User": String(uid), "Level": level, "Correct": numCorrect};
+        let user = JSON.parse(localStorage.getItem('userdata'));
+        let uid = user.uid;
+        console.log(uid);
+		let final_results = {"User": String(uid), "Level": level, "Correct": numCorrect};
 
-            console.log(final_results);
-            $('#display_results').text(numCorrect + "/10 for level: " + level);
-            $.ajax({
-                url: '/results',
-                data: JSON.stringify(final_results),
-                type: 'POST',
-                contentType: "application/json",
-                dataType: "json",
-                success: function(response) {
-                    console.log('submitted');
-                },
-                error: function(error) {
-                    console.log('ERROR');
-                    console.log(error);
-                }
-            });
-
-        }
-
+        console.log(final_results);
+        $('#display_results').text(numCorrect + "/10 for level: " + level);
+		$.ajax({
+            url: '/post_results',
+			data: JSON.stringify(final_results),
+            type: 'POST',
+			contentType: "application/json",
+			dataType: "json",
+            success: function(response) {
+                console.log('submitted');
+            },
+            error: function(error) {
+				console.log('ERROR');
+				console.log(error);
+            }
+        });
 
 	});
+
+
 });
